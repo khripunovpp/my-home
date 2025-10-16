31453d88b246de2bb04adb67e0e4beeecb1931d0
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2025-10-16 20:19:39 +0100

    venv connect

--- src/models/TemperatureSensorResult.py
+++ src/models/TemperatureSensorResult.py
@@ -5,8 +5,5 @@ class TemperatureSensorResult(BaseModel):
     temperature: float
     humidity: float
 
-    def __init__(self, temperature: float, humidity: float):
-        super().__init__(temperature=temperature, humidity=humidity)
-
     def is_hot(self) -> bool:
         return self.temperature > 25.0
--- subscribe.py
+++ subscribe.py
@@ -1,36 +1,20 @@
-import json
-
 import paho.mqtt.client as mqtt
-from pydantic import BaseModel
-
-from src.models.TemperatureSensorResult import TemperatureSensorResult
 
 # Параметры
-MQTT_BROKER = "localhost"
+MQTT_BROKER = "192.168.1.230"
 MQTT_PORT = 1883
-DEVICE_TOPIC = "zigbee2mqtt/temperature_sensor"  # замени на имя твоего устройства
-
-
+# DEVICE_TOPIC = "zigbee2mqtt/temperature_sensor"  # замени на имя твоего устройства
+DEVICE_TOPIC = "zigbee2mqtt/presence_sensor"  # замени на имя твоего устройства
 
 
 # Функции обратного вызова
 def on_connect(client, userdata, flags, rc):
-    print(f"Connected with result code {rc}")
+    print(f"Connected with result code {rc} : {DEVICE_TOPIC}")
     client.subscribe(DEVICE_TOPIC)
 
 
 def on_message(client, userdata, msg):
-    try:
-        payload = TemperatureSensorResult(msg.payload.decode())  # превращаем JSON-строку в dict
-    except json.JSONDecodeError:
-        print(f"Invalid JSON: {msg.payload.decode()}")
-        return
-    presence = payload.get("presence", False)
     print(f"Topic: {msg.topic}, Payload: {msg.payload.decode()}")
-    if presence:
-        print("Хуйло детектед!")
-    else:
-        print("No presence detected.")
 
 
 # Настройка клиента
